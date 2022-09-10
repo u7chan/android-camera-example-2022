5eb63b0630be05d7614ce96b932d86c33fbd83c3
una <unagamix@gmail.com>
2022-09-10 22:52:41 +0900

    fix

--- app/src/main/java/org/una/danger/android/android_camera_example_2022/MainActivity.kt
+++ app/src/main/java/org/una/danger/android/android_camera_example_2022/MainActivity.kt
@@ -9,21 +9,16 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import org.una.danger.android.android_camera_example_2022.ui.theme.Androidcameraexample2022Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Androidcameraexample2022Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+                Greeting("Android")
             }
         }
     }
@@ -32,12 +27,4 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Androidcameraexample2022Theme {
-        Greeting("Android")
-    }
 }
