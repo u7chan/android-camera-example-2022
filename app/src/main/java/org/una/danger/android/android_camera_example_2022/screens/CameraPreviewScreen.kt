9f7fc5084b72d4a7c5aab22fe49c7b9ef3e2a01f
una <unagamix@gmail.com>
2022-09-10 23:35:45 +0900

    feat: init code

--- app/src/main/java/org/una/danger/android/android_camera_example_2022/screens/CameraPreviewScreen.kt
+++ app/src/main/java/org/una/danger/android/android_camera_example_2022/screens/CameraPreviewScreen.kt
@@ -1,9 +1,46 @@
 package org.una.danger.android.android_camera_example_2022.screens
 
-import androidx.compose.material.Text
+import android.view.ViewGroup
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 
 @Composable
 fun CameraPreviewScreen() {
-    Text(text = "プレビュー画面")
+    val context = LocalContext.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    BoxWithConstraints {
+        AndroidView(
+            factory = { ctx ->
+                val scaleType = PreviewView.ScaleType.FILL_CENTER
+                val previewView = PreviewView(ctx).apply {
+                    this.scaleType = scaleType
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                    )
+                    implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                }
+                val executor = ContextCompat.getMainExecutor(ctx)
+                cameraProviderFuture.addListener(
+                    {
+                        // TODO
+                    },
+                    executor
+                )
+                previewView
+            },
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
